72497b6e1cf208c24e033704fe5fab70305eaea4
Pedro Afonso <pedroafonsoprogramador@gmail.com>
2025-02-14 10:59:16 -0300

    fix: Consertada um problema que não permitia o fetch de mais de uma conexão

--- internal/database/fetch.go
+++ internal/database/fetch.go
@@ -13,14 +13,27 @@ func FetchConnections() {
 		log.Fatalf("Failed to find database")
 	}
 	defer conn.Close(context.Background())
-	var name string
-	var instance_id string
-	var limit uint
-	err = conn.QueryRow(context.Background(), "SELECT name, instance_id, limite FROM instances WHERE active = true").Scan(&name, &instance_id, &limit)
+
+	rows, err := conn.Query(context.Background(), "SELECT name, instance_id, limite FROM instances WHERE active = true")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
 	}
-	fmt.Println(name, instance_id, limit)
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		var instance_id string
+		var limit uint
+		if err := rows.Scan(&name, &instance_id, &limit); err != nil {
+			fmt.Fprintf(os.Stderr, "Row scan with issues: %v\n ", err)
+			continue
+		}
+		fmt.Println(name, instance_id, limit)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatalf("Rows iteration error: %v\n", err)
+	}
 
 }
