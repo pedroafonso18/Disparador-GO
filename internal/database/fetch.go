b845b241f6a2faa0a7c05ac570259f2f821e78e6
Pedro Afonso <pedroafonsoprogramador@gmail.com>
2025-02-14 11:21:49 -0300

    feat: Adicionada a funcionalidade de busca de campanhas

--- internal/database/fetch.go
+++ internal/database/fetch.go
@@ -37,3 +37,32 @@ func FetchConnections() {
 	}
 
 }
+
+func FetchCampanhas() {
+	conn, err := GetConnection()
+	if err != nil {
+		log.Fatalf("Error at getting the campaigns.")
+	}
+	defer conn.Close(context.Background())
+
+	rows, err := conn.Query(context.Background(), "SELECT numero,campanha FROM campanhas WHERE disparado = FALSE")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error at getting query from db: %s\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var numero string
+		var campanha string
+		if err := rows.Scan(&numero, &campanha); err != nil {
+			fmt.Fprintf(os.Stderr, "Error at reading line from campanhas: %s\n", err)
+			continue
+		}
+		fmt.Printf(numero, campanha)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatalf("Error at row iteration: %v\n", err)
+	}
+}
